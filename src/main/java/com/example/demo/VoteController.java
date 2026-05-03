5f79e75ddabd37318c7233282dfb5e336ce06807
unknown <jserverings@gmail.com>
2026-05-03 15:39:16 +0800

    adalah pokoknya

--- src/main/java/com/example/demo/VoteController.java
+++ src/main/java/com/example/demo/VoteController.java
@@ -1,16 +1,16 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*") // penting biar HTML bisa akses
+@CrossOrigin(origins = "*")     
 public class VoteController {
 
     private int yes = 0;
